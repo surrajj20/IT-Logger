93fcf394d54c906953a613891a72719daabf7b53
surrajj20 <shah.suraj1200@gmail.com>
2020-03-05 19:40:57 +0530

    Tech option Select Option

--- src/components/logs/EditLogModel.js
+++ src/components/logs/EditLogModel.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -56,9 +57,8 @@ const EditLogModal = ({ current, updateLog }) => {
                             <option value='' disabled>
                                 Select Technician
                             </option>
-                            <option value='John Deo'>John Deo</option>
-                            <option value='Sam Smith'>Sam Smith</option>
-                            <option value='Sara Wilson'>Sara Wilson</option>
+                            <TechSelectOptions />
+
                         </select>
                     </div>
 
